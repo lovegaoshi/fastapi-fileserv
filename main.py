0704fb68ef1879baab4721679492c3e103967f71
Ubuntu <gaoshi@noxplay.us.to>
2023-05-30 23:26:05 +0000

    feat: vercel

--- main.py
+++ main.py
@@ -1,42 +1,32 @@
-from fastapi import FastAPI, HTTPException, Request, Header
-from typing import Annotated
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Response, HTTPException, Request
 from os.path import join as pjoin
-import logging
 from urllib.parse import unquote
-
+import vercelsql
 
 app = FastAPI()
 
-logging.basicConfig(level=logging.INFO)
 @app.get("/download/{userid}")
 async def download(userid: str):
     try:
-        return FileResponse(pjoin('jsons', userid + '.noxBackup'))
-    except BaseException as e:
-        logging.error(e)
+        return Response(content=vercelsql.get(userid))
+    except:
         raise HTTPException(status_code=406, detail="n/a")
 
 @app.post("/upload")
 async def upload(
     request: Request,
-    secret_key: Annotated[str | None, Header()] = None,
-    userid: Annotated[str | None, Header()] = None):
+    secret_key,
+    userid):
     try:
-        # my local fastapi can get header but not cloud..?
-        if secret_key is None: secret_key = request.headers.get('secret-key')
-        if userid is None: userid = request.headers.get('userid')
         # keep your cloud safe by setting a secret key. noxplayer uses noxplayer.
-        if secret_key != "noxplayer": raise Exception()
+        if secret_key != "noxplayer": raise Exception('wrong key')
         username = unquote(userid)
         # keep your cloud safe by setting that only your userid 
         # will be accepted!
-        if not username in ['王胡桃w']: raise Exception()
-        with open(pjoin('jsons', username + '.noxBackup'), 'wb') as f:
-            f.write(await request.body())
+        if not username in ['lovegaoshi']: raise Exception('wrong user')
+        vercelsql.save(username, bytes(await request.body()))
         return {"message": "Hello World"}
-    except BaseException as e:
-        logging.error(e)
+    except:
         raise HTTPException(status_code=406, detail="n/a")
 
 @app.get("/")
